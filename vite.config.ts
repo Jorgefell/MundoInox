ee5723284afa43f46e27efa0502efc74bcb14a57
Jorge Steven Nina Leandres <jorge.nina@tecsup.edu.pe>
2026-01-19 16:11:30 -0500

    Build GitHub Pages con base correcto

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/MundoInox/',   // 🔴 ESTO ES LO CLAVE
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
